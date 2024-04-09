9bb37e6648e3b97e80bc72d826fbfe2c610024a2
ddealsan <ddealsan@myuax.com>
2024-04-09 18:30:59 +0200

    avanzamos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import TextEditor.TextEditor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TextEditor editor = new TextEditor();
+
     }
 }
