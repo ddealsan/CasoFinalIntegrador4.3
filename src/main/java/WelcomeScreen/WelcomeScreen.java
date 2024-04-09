74cebdcd19101b346e3745e54e44c88bb221c651
ddealsan <ddealsan@myuax.com>
2024-04-09 19:04:18 +0200

    avanzamos

--- src/main/java/WelcomeScreen/WelcomeScreen.java
+++ src/main/java/WelcomeScreen/WelcomeScreen.java
@@ -16,18 +16,26 @@ public class WelcomeScreen {
         JButton newButton = new JButton("Crear nuevo texto");
         newButton.setPreferredSize(new Dimension(100, 50));
         newButton.addActionListener(e -> new TextEditor());
+        newButton.setForeground(Color.WHITE);
+        newButton.setBackground(Color.BLUE);
 
         JButton editButton = new JButton("Editar texto existente");
         editButton.setPreferredSize(new Dimension(100, 50));
         editButton.addActionListener(e -> new TextEditor2());
+        editButton.setForeground(Color.WHITE);
+        editButton.setBackground(Color.BLUE);
 
         JButton compareButton = new JButton("Comparador de textos");
         compareButton.setPreferredSize(new Dimension(100, 50));
         compareButton.addActionListener(e -> new TextComparator());
+        compareButton.setForeground(Color.WHITE);
+        compareButton.setBackground(Color.BLUE);
 
         JButton exitButton = new JButton("Salir");
         exitButton.setPreferredSize(new Dimension(100, 50));
         exitButton.addActionListener(e -> System.exit(0));
+        exitButton.setForeground(Color.WHITE);
+        exitButton.setBackground(Color.BLUE);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(4, 1, 10, 10));
@@ -41,18 +49,20 @@ public class WelcomeScreen {
 
         JLabel welcomeLabel = new JLabel("<html><div style='text-align: center;'>Bienvenido al sistema de publicaciones de texto de la Universidad Alfonso X El Sabio</div></html>");
         welcomeLabel.setForeground(Color.BLUE);
-        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        welcomeLabel.setFont(new Font("Fénix", Font.BOLD, 20));
         welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
 
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BorderLayout());
         leftPanel.add(imageLabel, BorderLayout.CENTER);
         leftPanel.add(welcomeLabel, BorderLayout.SOUTH);
+        leftPanel.setBackground(Color.WHITE);
 
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, buttonPanel);
         splitPane.setDividerSize(10);
         splitPane.setDividerLocation(400);
         splitPane.setEnabled(false);
+        splitPane.setForeground(Color.BLUE);
 
         frame.add(splitPane);
         frame.setSize(800, 600);
